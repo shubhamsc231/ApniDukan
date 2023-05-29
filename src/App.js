539f6a0aaffecba1eaaaa67870eed3099b3023b2
shubham-hcah <128469359+shubhamchopra-hcah@users.noreply.github.com>
2023-05-29 17:12:16 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <h1>Apni Dukan</h1>
-    </div>
+    <>
+    <Header/>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+    return
+    <div>
+        <h1>Apni Dukan</h1>
+    </div>
+}
+
+export default Header;
