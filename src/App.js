dfe913f7f1f43bac7c0ff4fa954b4571bd388ec4
shubham-hcah <128469359+shubhamchopra-hcah@users.noreply.github.com>
2023-06-20 00:56:59 +0530

    hidden comment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import Home from './pages/Home';
-import { ReactDOM } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './pages/Login.js';
-import Layout from './pages/Layout.js';
+//import { ReactDOM } from 'react-dom/client';
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import Login from './pages/Login.js';
+//import Layout from './pages/Layout.js';
 function App() {
   return (
     <>
